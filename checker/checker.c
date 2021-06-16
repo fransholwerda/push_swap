d3c54a25e5c62a1ea1704c5e9208360361830e43
Frans Holwerda <fransholwerda@Franss-MacBook-Pro.local>
2021-06-16 21:45:36 +0200

    deleted o files

--- checker/checker.c
+++ checker/checker.c
@@ -6,7 +6,7 @@
 /*   By: fholwerd <fholwerd@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/05/27 15:43:52 by fholwerd      #+#    #+#                 */
-/*   Updated: 2021/05/27 18:02:47 by fholwerd      ########   odam.nl         */
+/*   Updated: 2021/05/30 17:58:07 by fholwerd      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,23 @@ static t_numbers	*fill_numbers(int argc, char **argv)
 	t_numbers	*num;
 	int			i;
 	
-	if (argc > 1)
-		num = lst_new(ft_atoi(argv[1]));
 	i = 2;
+	num = NULL;
 	while (i < argc)
 	{
+		if (!ft_isint(argv[i]) || argc < 2)
+		{
+			printf("test1\n");
+			write(1, "Wrong input, provide numbers.\n", 30);
+			return (NULL);
+		}
+		printf("test2, argv: %s\n", argv[i]);
 		lst_add_back(num, ft_atoi(argv[i]));
 		i++;
 	}
+	printf("test3 num: %d\n", num->data);
 	lst_loop(num);
+	printf("test4\n");
 	return (num);
 }
 
@@ -34,11 +42,13 @@ int	checker(int argc, char **argv)
 	t_numbers	*num;
 	int			i;
 
+	printf("test0\n");
 	num = fill_numbers(argc, argv);
+	printf("test\n");
 	if (!num)
 		return (-1);
 	i = 0;
-	while (i < 9)
+	while (i < argc)
 	{
 		printf("'%d'\n", num->data);
 		num = num->next;
--- checker/struct_utility.c
+++ checker/struct_utility.c
@@ -6,7 +6,7 @@
 /*   By: fholwerd <fholwerd@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/05/27 16:37:00 by fholwerd      #+#    #+#                 */
-/*   Updated: 2021/05/27 16:58:52 by fholwerd      ########   odam.nl         */
+/*   Updated: 2021/05/30 17:55:26 by fholwerd      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,11 +27,16 @@ void	lst_add_back(t_numbers *list, int data)
 {
 	t_numbers	*new_list;
 
-	while (list->next)
-		list = list->next;
-	new_list = lst_new(data);
-	new_list->prev = list;
-	list->next = new_list;
+	if (!list)
+		list = lst_new(data);
+	else
+	{
+		while (list->next)
+			list = list->next;
+		new_list = lst_new(data);
+		new_list->prev = list;
+		list->next = new_list;
+	}
 }
 
 void	lst_loop(t_numbers *list)
@@ -39,8 +44,14 @@ void	lst_loop(t_numbers *list)
 	t_numbers	*list_first;
 
 	list_first = list;
+	printf("test4\n");
 	while (list->next)
+	{
+		printf("test5\n");
 		list = list->next;
+		printf("test5.5\n");
+	}
+	printf("test6\n");
 	list->next = list_first;
 	list_first->prev = list;
 }
