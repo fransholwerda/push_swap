99e5f56acd074c3182448e17f35eb765845aa938
Frans Holwerda <fransholwerda@Franss-MacBook-Pro.local>
2021-08-04 12:39:30 +0200

    added support for 1 argument list

--- parse.c
+++ parse.c
@@ -6,7 +6,7 @@
 /*   By: fholwerd <fholwerd@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/06/22 16:59:49 by fholwerd      #+#    #+#                 */
-/*   Updated: 2021/06/30 13:34:03 by fholwerd      ########   odam.nl         */
+/*   Updated: 2021/08/04 12:14:36 by fholwerd      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,20 +17,44 @@ t_numbers	*fill_numbers(int argc, char **argv)
 	t_numbers	*num;
 	int			i;
 
-	i = 1;
 	num = NULL;
-	while (i < argc)
+	if (argc > 2 || (argc == 2 && ft_isint(argv[1])))
 	{
-		if ((!ft_isint(argv[i]) && (argv[i][0] != '-')) || argc < 2)
+		i = 1;
+		while (i < argc)
 		{
-			write(1, "Error\n", 6);
-			return (NULL);
+			if ((!ft_isint(argv[i]) && (argv[i][0] != '-')) || argc < 2)
+			{
+				write(1, "Error\n", 6);
+				return (NULL);
+			}
+			if (!num)
+				num = lst_new(ft_atoi(argv[i]));
+			else
+				lst_add_back(num, ft_atoi(argv[i]));
+			i++;
 		}
-		if (i == 1)
-			num = lst_new(ft_atoi(argv[i]));
-		else
-			lst_add_back(num, ft_atoi(argv[i]));
-		i++;
+	}
+	else
+	{
+		i = 0;
+		while (ft_isdigit(argv[1][i]) || argv[1][i] == ' ' || argv[1][i] == '-')
+		{
+			if (ft_isdigit(argv[1][i]) || argv[1][i] == '-')
+			{
+				if (!num)
+					num = lst_new(ft_atoi(&argv[1][i]));
+				else
+					lst_add_back(num, ft_atoi(&argv[1][i]));
+				if (argv[1][i] == '-' && !ft_isdigit(argv[1][i + 1]))
+				{
+					write(1, "Error\n", 6);
+					return (NULL);
+				}
+				while (ft_isdigit(argv[1][i]) || argv[1][i] == '-')
+					i++;
+			}
+		}
 	}
 	lst_loop(num);
 	return (num);
